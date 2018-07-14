30f756df4a051a6e9732621ea1a4f4a4946a371f
Divikiran Ravela <divikiran1@gmail.com>
2018-07-14 14:18:32 +1000

    Initla checkin

---
+++ MvvmSample/MvvmSample/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MvvmSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainViewModel VM
+        {
+            get;
+            set;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = VM = new MainViewModel();
+            VM.Name = "Canbera";
+
+        }
+
+        protected override void OnAppearing()
+        {
+            VM.Name = "Syndey";
+            base.OnAppearing();
+        }
+    }
+}
---
+++ MvvmSample/MvvmSample/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MvvmSample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public MainViewModel()
+        {
+            Name = "Melbourne";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+   
